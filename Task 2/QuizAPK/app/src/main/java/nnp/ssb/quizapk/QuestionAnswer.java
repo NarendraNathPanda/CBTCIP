1e67e78efc0d2b856c94d699c9bf2b1c07571d14
NARENDRA NATH PANDA <93674416+NarendraNathPanda@users.noreply.github.com>
2023-08-28 21:01:55 +0530

    task 2
    Task 2 has been submitted.

---
+++ Task 2/QuizAPK/app/src/main/java/nnp/ssb/quizapk/QuestionAnswer.java
@@ -0,0 +1,44 @@
+package nnp.ssb.quizapk;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which of the following is not a Java features?",
+            "________ is the full form Wifi.",
+            "The \u0021 article referred to as a",
+            "________ is used to find and fix bugs in the Java programs.",
+            "Which of the following is a valid declaration of a char?",
+            "What is the return type of the hashCode() method in the Object class?",
+            "Which of the following is a valid long literal?",
+            "What does the expression float a = 35 / 0 return?",
+            "Evaluate the following Java expression, if x=3, y=5, and z=10:",
+            " Which of the following tool is used to generate API documentation in HTML format from doc comments in source code?"
+    };
+
+    public static String choices[][] = {
+            {"Use of pointers","Dynamic","Architecture Neutral","Object-oriented"},
+            {"Wireless Focus","Wireless Fidelity","Wired Fidelity","Wired Focus"},
+            {"Unicode escape sequence","Octal escape","Hexadecimal","Line feed"},
+            {"JVM","JRE","JDK","JDB"},
+            {"char ch = '\\utea';","char ca = 'tea';", "char cr = \\u0223;","char cc = '\\itea';"},
+            {"Object","int","long","void"},
+            {"ABH8097","L990023","904423","0xnf029L"},
+            {"0","Not a Number","Infinity","Run time exception"},
+            {"24","23","20","25"},
+            {"javap tool","javaw command","Javadoc tool","javah command"}
+    };
+
+    public static String correctAnswers[] = {
+            "Use of pointers",
+            "Wireless Fidelity",
+            "Unicode escape sequence",
+            "JDB",
+            "char ch = '\\utea';",
+            "int",
+            "0xnf029L",
+            "Infinity",
+            "25",
+            "Javadoc tool"
+    };
+
+}
